661853a5e75e4501117be6218deab141a2fa7b4d
Vipershot <imvipershot@gmail.com>
2024-11-19 18:36:03 -0500

    feat(details): add detail award

--- src/components/molecules/Details/Details.tsx
+++ src/components/molecules/Details/Details.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { IItemCarousel } from '../../../interface/ICarousel';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { IItemCarousel } from "../../../interface/ICarousel";
+import Tickets from "../Tickets/Tickets";
 
 interface CarouselDetailsProps {
   dataTest: IItemCarousel[];
@@ -9,20 +10,27 @@ interface CarouselDetailsProps {
 const CarouselDetails: React.FC<CarouselDetailsProps> = ({ dataTest }) => {
   const { id } = useParams<{ id: string }>();
 
-  const carouselItem = dataTest.find(item => item.id === id);
+  const carouselItem = dataTest.find((item) => item.id === id);
 
   if (!carouselItem) {
     return <div>No se encontró el carusel con el id proporcionado</div>;
   }
 
   return (
-    <div>
-      <h2>{carouselItem.title}</h2>
-      <p>Precio: ${carouselItem.price}</p>
-      <p>Descripción: {carouselItem.description}</p>
-      <p> Fecha: {carouselItem.date}</p>
-      <img src={carouselItem.imgUrl} alt={carouselItem.title} />
+    <>
+    <div className="flex justify-center gap-2">
+      <div className="w-[50%] h-[600px]">
+        <img src={carouselItem.imgUrl} alt={carouselItem.title} className="w-full h-full object-cover sm:object-contain"/>
+      </div>
+      <div className="w-[40%]">
+        <h2 className="text-dark font-bold text-xl">{carouselItem.title}</h2>
+        <p>Precio: ${carouselItem.price}</p>
+        <p className="text-dark">Descripción: {carouselItem.description}</p>
+        <p className="text-dark"> Fecha: {carouselItem.date}</p>
+      </div>
     </div>
+    <Tickets/>
+    </>
   );
 };
 
---
+++ src/components/molecules/Tickets/Tickets.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import createNumbersArray from "../../../utils/testTickets";
+
+const Tickets = () => {
+  const numbers = createNumbersArray();
+  const numbersState = numbers.map(item=> ({state:false,number:item}))
+  const [numberss, setNumberss] = useState(numbersState)
+
+  const handleChecked = (selectedNumber: number) => {
+    // Actualizar el estado del checkbox seleccionado
+    const updatedNumbers = numberss.map((item) =>
+      item.number === selectedNumber
+        ? { ...item, state: !item.state } // Cambiar el estado del checkbox
+        : item
+    );
+    setNumberss(updatedNumbers); // Actualizar el estado
+  };
+  return (
+    <div className="flex flex-wrap gap-2" >
+      {numberss.map((data, i) => (
+     <label htmlFor={`ticket-${data.number}`} style={{ position: "relative", display: "inline-block", width: "40px", height: "40px" }}>
+     <input 
+checked={data.state}
+onChange={()=> handleChecked(data.number)}
+     id={`ticket-${data}`}    
+     style={{
+        appearance:"none",
+        backgroundColor: data.state ? "#007bff" : "#fff",
+          width: "100%",
+          height: "100%",
+          border: "2px solid #000",
+          borderRadius: "4px",
+          cursor: "pointer",
+        }} type="checkbox" value={data.number} />
+     <span     style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          fontSize: "14px",
+          pointerEvents: "none", // El texto no debe interferir con el clic
+        }}>{data.number}</span>
+   </label>
+      ))}
+    </div>
+  );
+};
+
+export default Tickets;
