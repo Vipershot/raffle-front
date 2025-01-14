73de884c7e3cbd651fc26281204bf87111af808b
Vipershot <imvipershot@gmail.com>
2025-01-14 13:08:20 -0500

    loterry with winner

--- src/components/organims/Lottery/Lottery.tsx
+++ src/components/organims/Lottery/Lottery.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { IAward } from "../../../interface/awards";
 import { getAward } from "../../../services/awards";
 import useCountdown from "../../../hooks/useCountdown";
+import Confetti from "react-confetti";
 
 const formatTime = (seconds: number) => {
   const hours = Math.floor((seconds % (3600 * 24)) / 3600);
@@ -14,23 +15,33 @@ const formatTime = (seconds: number) => {
 const Lottery = () => {
   const { id } = useParams();
   const [award, setAward] = useState({} as IAward);
-  const [endDate, setEndDate] = useState(new Date("2025-01-15T00:00:00Z"));
+  const [endDate, setEndDate] = useState(new Date(2025, 0, 14, 11, 3, 0));
   const { isCountdownActive, timer } = useCountdown(endDate);
+  const [showConfetti, setShowConfetti] = useState(false);
+  const [winnerSelected, setWinnerSelected] = useState(false);
 
   useEffect(() => {
     getAward(id as string).then((award: IAward) => {
       setAward(award);
-      setEndDate(new Date("2025-01-15T00:00:00Z"));
+      setEndDate(new Date(award.endDate));
     });
   }, [id]);
 
+  useEffect(() => {
+    if (isCountdownActive && timer === 0) {
+      setShowConfetti(true);
+      setWinnerSelected(true);
+    }
+  }, [timer, isCountdownActive]);
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen max-w-[100%] overflow-hidden">
+      {showConfetti && <Confetti style={{ width: "100%", height: "100%" }} />}
       <h1 className="text-[24px] md:text-[30px] text-center">{award.title}</h1>
       <div className="w-[200px] h-auto md:w-[300px] overflow-hidden my-4">
         <img src={award.cover} alt="" />
       </div>
-      {isCountdownActive ? (
+      {isCountdownActive && !winnerSelected ? (
         <>
           <h2 className="text-[22px] md:text-[28px] text-center text-primary font-bold">
             Tiempo para sortear al ganador:
@@ -47,8 +58,22 @@ const Lottery = () => {
             <span>Segundos</span>
           </p>
         </>
+      ) : winnerSelected ? (
+        <>
+          <h2 className="text-[22px] md:text-[28px] text-center text-primary font-bold">
+            Ganador seleccionado
+          </h2>
+          <p className="md:text-[26px] text-[20px]">Número de ticket ganador</p>
+          <p className="md:text-[28px] text-[20px] mt-6 font-bold text-primary bg-primary text-white rounded-full w-16 h-16 flex items-center justify-center">
+            123
+          </p>
+        </>
       ) : (
-        <p className="text-[20px] md:text-[26px]">No hay sorteo hoy</p>
+        <>
+          <p className="text-[20px] md:text-[26px]">
+            {!isCountdownActive ? "No hay sorteo hoy" : ""}
+          </p>
+        </>
       )}
     </div>
   );
