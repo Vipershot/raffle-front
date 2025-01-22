555a091b2582c05a2bca9f293ade5f5b12fb01dc
msanchez19 <msanchez@theegg.io>
2025-01-22 16:37:33 -0400

    env config

--- src/components/organims/Popover/DetailProfile/DetailProfile.tsx
+++ src/components/organims/Popover/DetailProfile/DetailProfile.tsx
@@ -17,9 +17,9 @@ export const DetailProfile =({profile, handlePopover}: Props)=>{
     </div>
    
 
-<div className="flex items-center space-x-4 mb-4">
+<div className="flex items-center space-x-2 mb-4">
   <div className="w-8 h-8 bg-info rounded-full flex items-center justify-center text-white">
-    <RxAvatar size={30} />
+    <img src={`https://ui-avatars.com/api/?name=${profile.name}&background=random`} alt="" width={100} className="w-8 h-8 bg-info rounded-full flex items-center justify-center text-white"/>
   </div>
   <div className="flex-grow">
     <p className="text-sm font-medium text-gray-800">{profile.name}</p>
